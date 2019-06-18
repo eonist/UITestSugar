d85cbec62367f9ebd57e5bfb87fb3a91c5f5cce8
andre.jorgensen <andre.jorgensen@tidal.com>
2019-06-18 10:10:13 +0200

    Use correct framework search paths

---
+++ UITestSugar/UITestSugar.h
@@ -0,0 +1,19 @@
+//
+//  UITestSugar.h
+//  UITestSugar
+//
+//  Created by André Jørgensen on 18/06/2019.
+//  Copyright © 2019 futurelab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITestSugar.
+FOUNDATION_EXPORT double UITestSugarVersionNumber;
+
+//! Project version string for UITestSugar.
+FOUNDATION_EXPORT const unsigned char UITestSugarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITestSugar/PublicHeader.h>
+
+
---
+++ UITestSugarExampleUITests/UITestSugarExampleUITests.swift
@@ -0,0 +1,35 @@
+import XCTest
+import UITestSugar
+
+class UITestSugarExampleUITests: XCTestCase {
+   
+   override func setUp() {
+      
+      let homeNavButton = NavigationHelper.tabButton(tabButtonName: "home")
+      XCUIApplication().descendants(matching: .any).hittableElements.count
+      
+      let app = XCUIApplication()
+      let searchedElement = app.filterElements(containing: "Sugar", "500 g").element
+      searchedElement.exists // true , false
+      searchedElement.firstMatch.tap()
+      
+      // Put setup code here. This method is called before the invocation of each test method in the class.
+      
+      // In UI tests it is usually best to stop immediately when a failure occurs.
+      continueAfterFailure = false
+      
+      // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+      XCUIApplication().launch()
+      
+      // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+   }
+   
+   override func tearDown() {
+      // Put teardown code here. This method is called after the invocation of each test method in the class.
+   }
+   
+   func testExample() {
+      // Use recording to get started writing UI tests.
+      // Use XCTAssert and related functions to verify your tests produce the correct results.
+   }
+}
