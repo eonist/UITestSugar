c23f92fef1cc125e3702a5e657917cb5cb4ec6df
andre.jorgensen <andre.jorgensen@tidal.com>
2019-06-19 14:08:28 +0200

    Update project src

---
+++ UITestSugarIOS/UITestSugarIOS.h
@@ -0,0 +1,19 @@
+//
+//  UITestSugarIOS.h
+//  UITestSugarIOS
+//
+//  Created by André Jørgensen on 19/06/2019.
+//  Copyright © 2019 futurelab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITestSugarIOS.
+FOUNDATION_EXPORT double UITestSugarIOSVersionNumber;
+
+//! Project version string for UITestSugarIOS.
+FOUNDATION_EXPORT const unsigned char UITestSugarIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITestSugarIOS/PublicHeader.h>
+
+
