e99b73a522521ff1ce248d0ec86ac9c2310fe53c
Eonist <30n1st@gmail.com>
2019-06-23 14:34:06 +0200

    Minor refactor

--- src/ui/element/ElementAsserter.swift
+++ src/ui/element/ElementAsserter.swift
@@ -3,23 +3,32 @@ import XCTest
 
 public class ElementAsserter {
    /**
-    * Fixme: ⚠️️ write doc
+    * Asserts if an element is visible on screen
+    * Fixme: ⚠️️ write example
     */
    public static func visible(element: XCUIElement) -> Bool {
       guard element.exists && !element.frame.isEmpty else { return false }
       return XCUIApplication().windows.element(boundBy: 0).frame.contains(element.frame)
    }
    /**
+    * Asserts if an element exists
     * @Example:
     * XCTAssertTrue(ElementAsserter(element: app.buttons[“Sign up”]).exists) // ✅
     */
-   func exists(element: XCUIElement) -> Bool {
+   public static func exists(element: XCUIElement) -> Bool {
       return element.exists
    }
    /**
-    * 
+    * Asserts if all elements in an array exists
     */
-   func allExists(elements: [XCUIElement]) -> Bool {
+   public static func allExists(elements: [XCUIElement]) -> Bool {
       return elements.first { !$0.exists } == nil
+   }
+   /**
+    * ## Examples
+    * hasText(element: app.alerts.element,"Please enter a valid email address")
+    */
+   public static func hasText(element: XCUIElement, text: String) -> Bool {
+      return element.staticTexts[text].exists
    }
 }
