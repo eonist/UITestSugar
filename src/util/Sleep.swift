2e35f5b4ead488a1f94bac620254d1575a7c4ac6
Eonist <30n1st@gmail.com>
2019-06-23 09:44:12 +0200

    Minor reorg

--- src/util/Sleep.swift
+++ src/util/Sleep.swift
@@ -1,9 +1,9 @@
 import Foundation
 /**
- * Supports fractional time
+ * Supports fractional timeb(normal sleep only supports integer)
  * ## Examples:
  * sleep(sec: 2.2) // sleeps for 2.2 seconds
  */
-public func sleep(sec: Double){
-    usleep(useconds_t(sec * 1000000)) // wait for n secs
+public func sleep(sec: Double) {
+    usleep(useconds_t(sec * 1_000_000)) // wait for n secs
 }
