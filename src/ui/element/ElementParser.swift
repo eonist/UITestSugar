5fc0e8ab1de1e4f9ea6c8eda3560557737e6ec10
andre.jorgensen <andre.jorgensen@tidal.com>
2019-06-24 10:09:22 +0200

    Add debug method

--- src/ui/element/ElementParser.swift
+++ src/ui/element/ElementParser.swift
@@ -19,4 +19,19 @@ public class ElementParser {
       let query: XCUIElementQuery = element.descendants(matching: type)
       return QueryParser.elements(query: query)
    }
+   /**
+    * Helps identify elements (Useful when there is localization involved)
+    * ## Examples:
+    * debugChildren(query: app.scrollViews.otherElements.buttons)
+    * - Note: to debug descendants use the descendants call in the query
+    */
+   public static func debug(query: XCUIElementQuery) {
+      let elements = query.allElementsBoundByIndex
+      elements.forEach {
+         Swift.print("$0.identifier:  \($0.identifier)")
+         Swift.print("$0.accessibilityLabel:  \(String(describing: $0.accessibilityLabel))")
+         Swift.print("$0.label:  \($0.label)")
+         Swift.print("$0.elementType:  \($0.elementType.rawValue)")
+      }
+   }
 }
