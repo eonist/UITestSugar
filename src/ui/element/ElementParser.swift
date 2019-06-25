4e0da3aedf223da05bb1d70fa27e0a19b9e3f879
andre.jorgensen <andre.jorgensen@tidal.com>
2019-06-25 14:21:34 +0200

    Try to fix enum issue with debugHierarchy PT.2

--- src/ui/element/ElementParser.swift
+++ src/ui/element/ElementParser.swift
@@ -33,7 +33,7 @@ public class ElementParser {
     * Helps debug an element
     */
    public static func debug(element: XCUIElement, indentation: String = "") {
-      Swift.print("\(indentation)identifier:  \(element.identifier) accessibilityLabel:  \(String(describing: element.accessibilityLabel)) label:  \(element.label) elementType:  \(String(describing: XCUIElement.ElementType.init(rawValue:  element.elementType.rawValue))) title:  \(element.title)")
+      Swift.print("\(indentation)identifier:  \(element.identifier) accessibilityLabel:  \(String(describing: element.accessibilityLabel)) label:  \(element.label) type:  \(String(describing: XCUIElement.ElementType.init(rawValue:  element.elementType.rawValue))) title:  \(element.title)")
    }
    /**
     * Helps debug a hierarchy
@@ -49,5 +49,102 @@ public class ElementParser {
          debug(element: $0, indentation: identation)
          debugHierarchy(element: $0, type: type, indentationLevel: indentationLevel) // keep traversing down the hierarchy
       }
+   }
+}
+/**
+ * Helps identify ElementType (some bug in apples code prevents this with regular String(describing:))
+ */
+extension XCUIElement.ElementType {
+   enum ElementTypeName : String, CaseIterable {
+      case other
+      case application
+      case group
+      case window
+      case sheet
+      case drawer
+      case alert
+      case dialog
+      case button
+      case radioButton
+      case radioGroup
+      case checkBox
+      case disclosureTriangle
+      case popUpButton
+      case comboBox
+      case menuButton
+      case toolbarButton
+      case popover
+      case keyboard
+      case key
+      case navigationBar
+      case tabBar
+      case tabGroup
+      case toolbar
+      case statusBar
+      case table
+      case tableRow
+      case tableColumn
+      case outline
+      case outlineRow
+      case browser
+      case collectionView
+      case slider
+      case pageIndicator
+      case progressIndicator
+      case activityIndicator
+      case segmentedControl
+      case picker
+      case pickerWheel
+      case `switch`
+      case toggle
+      case link
+      case image
+      case icon
+      case searchField
+      case scrollView
+      case scrollBar
+      case staticText
+      case textField
+      case secureTextField
+      case datePicker
+      case textView
+      case menu
+      case menuItem
+      case menuBar
+      case menuBarItem
+      case map
+      case webView
+      case incrementArrow
+      case decrementArrow
+      case timeline
+      case ratingIndicator
+      case valueIndicator
+      case splitGroup
+      case splitter
+      case relevanceIndicator
+      case colorWell
+      case helpTag
+      case matte
+      case dockItem
+      case ruler
+      case rulerMarker
+      case grid
+      case levelIndicator
+      case cell
+      case layoutArea
+      case layoutItem
+      case handle
+      case stepper
+      case tab
+      case touchBar
+      case statusItem
+   }
+}
+extension XCUIElement.ElementType {
+   /**
+    * XCUIElement.ElementType.button.string // button
+    */
+   public var string: String {
+      return ElementTypeName.allCases[Int(self.rawValue)].rawValue
    }
 }
