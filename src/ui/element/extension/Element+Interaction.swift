a4180b76fbab7f9f996b3f0d5296a8f1ced29e3d
andre.jorgensen <andre.jorgensen@tidal.com>
2019-08-25 20:24:44 +0200

    Add more convenient returns of self in xcuielement extensions

--- src/ui/element/extension/Element+Interaction.swift
+++ src/ui/element/extension/Element+Interaction.swift
@@ -8,8 +8,10 @@ extension XCUIElement {
     * Removes any current text in the field before typing in the new value
     * - Parameter text: the text to enter into the field
     */
-   public func clearAndTypeText(text: String) {
+   @discardableResult
+   public func clearAndTypeText(text: String) -> XCUIElement {
       ElementModifier.clearAndTypeText(element: self, text: text)
+      return self
    }
 }
 /**
@@ -19,14 +21,18 @@ extension XCUIElement {
    /**
     * Scrolling
     */
-   public func scrollTo(dir: ElementModifier.Direction, searchCondition: ElementParser.MatchCondition) {
+    @discardableResult
+   public func scrollTo(dir: ElementModifier.Direction, searchCondition: ElementParser.MatchCondition) -> XCUIElement {
       ElementModifier.scrollTo(element: self, dir: dir, searchCondition: searchCondition)
+      return self
    }
    /**
     * Scrolling (beta)
     */
-   public func scrollToElement(element: XCUIElement) {
+    @discardableResult
+   public func scrollToElement(element: XCUIElement) -> XCUIElement {
       ElementModifier.scrollToElement(parent: self, element: element)
+      return self
    }
 }
 /**
@@ -36,13 +42,15 @@ extension XCUIElement {
    /**
     * Helps to tap things that doesn't work with regular .tap() calls. as .tap() calls must be on .isHittable items
     */
-   public func forceTapElement() {
+    @discardableResult
+   public func forceTapElement() -> XCUIElement {
       if self.isHittable {
          self.tap()
       } else {
          let coordinate: XCUICoordinate = self.coordinate(withNormalizedOffset: .init(dx: 0.0, dy: 0.0))
          coordinate.tap()
       }
+      return self
    }
    /**
     * Adds convenient way to tap and then wait for a duration (seconds)
