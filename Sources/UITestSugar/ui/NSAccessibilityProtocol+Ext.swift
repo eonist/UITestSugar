9e0f2ff29ebaedfc731e29d3befd37dc5bcccdc4
eonist <30n1st@gmail.com>
2022-11-26 06:18:44 +0100

    Create NSAccessibilityProtocol+Ext.swift

---
+++ Sources/UITestSugar/ui/NSAccessibilityProtocol+Ext.swift
@@ -0,0 +1,15 @@
+//#if os(macOS)
+//import Cocoa
+////import XCTest
+//
+//extension NSAccessibilityProtocol { // NSView
+//   public var accessibilityID: String? {
+//      get {
+//         self.accessibilityIdentifier()
+//      }
+//      set {
+//         self.setAccessibilityIdentifier(newValue)
+//      }
+//   }
+//}
+//#endif
