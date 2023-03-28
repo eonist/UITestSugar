bf704128c7a1ab1f4c5d52dee8a4d96199adf42f
eonist <30n1st@gmail.com>
2023-03-28 07:05:25 +0200

    Update ScreenShotMaker.swift

--- Sources/UITestSugar/util/ScreenShotMaker.swift
+++ Sources/UITestSugar/util/ScreenShotMaker.swift
@@ -2,20 +2,26 @@ import Foundation
 #if canImport(XCTest)
 import XCTest
 
-public final class ScreenShotMaker {
+private final class ScreenShotMaker {
 	/**
 	 * - Remark: You can find screenshots in Xcode -> Report navigator -> select your test
 	 * - Remark: Or go to: DerivedData -> ProjectName... -> Logs -> Test -> find .xcresult -> Show Package Contents -> Attachments
 	 * - Remark: Or search for the file `Screenshot` in deriveddata root folder
 	 * - Note: ref: https://stackoverflow.com/a/56345842/5389500
 	 * ## Examples:
-	 * ScreenShotMaker.makeScreenShot() // Put this line in your UITests where you want the screenshot to be taken
+	 * ScreenShotMaker.makeScreenShot(testCase: self) // Put this line in your UITests where you want the screenshot to be taken
 	 */
-	public static func makeScreenShot(testCase: XCTestCase) {
+	private static func makeScreenShot(app: XCUIApplication, name: String) { // of app: XCUIApplication,
     	let screenshot = XCUIScreen.main.screenshot()
-    	let fullScreenshotAttachment = XCTAttachment(screenshot: screenshot)
-    	fullScreenshotAttachment.lifetime = .keepAlways
-    	testCase.add(fullScreenshotAttachment)
+		 // let screenshot = app.windows.firstMatch.screenshot()
+    	let attachment = XCTAttachment(screenshot: screenshot)
+		#if os(iOS)
+        attachment.name = "Screenshot-\(name)-\(UIDevice.current.name).png"
+        #else
+        attachment.name = "Screenshot-\(name)-macOS.png"
+        #endif
+    	attachment.lifetime = .keepAlways
+    	app.add(attachment)
 	}
 }
 #endif
