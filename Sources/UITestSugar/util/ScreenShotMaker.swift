aa93c551a6246e77b5eec65728a395c06a1ef2bf
eonist <30n1st@gmail.com>
2023-03-28 09:54:19 +0200

    Update ScreenShotMaker.swift

--- Sources/UITestSugar/util/ScreenShotMaker.swift
+++ Sources/UITestSugar/util/ScreenShotMaker.swift
@@ -2,26 +2,32 @@ import Foundation
 #if canImport(XCTest)
 import XCTest
 
-private final class ScreenShotMaker {
-	/**
-	 * - Remark: You can find screenshots in Xcode -> Report navigator -> select your test
-	 * - Remark: Or go to: DerivedData -> ProjectName... -> Logs -> Test -> find .xcresult -> Show Package Contents -> Attachments
-	 * - Remark: Or search for the file `Screenshot` in deriveddata root folder
-	 * - Note: ref: https://stackoverflow.com/a/56345842/5389500
-	 * ## Examples:
-	 * ScreenShotMaker.makeScreenShot(testCase: self) // Put this line in your UITests where you want the screenshot to be taken
-	 */
-	private static func makeScreenShot(app: XCUIApplication, name: String) { // of app: XCUIApplication,
-    	let screenshot = XCUIScreen.main.screenshot()
-		 // let screenshot = app.windows.firstMatch.screenshot()
-    	let attachment = XCTAttachment(screenshot: screenshot)
-		#if os(iOS)
-        attachment.name = "Screenshot-\(name)-\(UIDevice.current.name).png"
-        #else
-        attachment.name = "Screenshot-\(name)-macOS.png"
-        #endif
-    	attachment.lifetime = .keepAlways
-    	//app.add(attachment)
-	}
+public final class ScreenShotMaker {
+   /**
+    * - Remark: You can find screenshots in Xcode -> Report navigator -> select your test
+    * - Remark: Or go to: DerivedData -> ProjectName... -> Logs -> Test -> find .xcresult -> Show Package Contents -> Attachments
+    * - Remark: Or search for the file `Screenshot` in deriveddata root folder
+    * - Note: Ref: https://stackoverflow.com/a/56345842/5389500
+    * - Note: There is a xcode gallary feature to browse screenshots see: https://stackoverflow.com/a/74678917/5389500
+    * Take a screenshot of a given app and add it to the test attachements.
+    * - Parameters:
+    *   - testCase: Needed to add the attachment
+    *   - app: The app to take a screenshot of.
+    *   - name: The name of the screenshot.
+    * ## Examples:
+    * ScreenShotMaker.makeScreenShot(testCase: self) // Put this line in your UITests where you want the screenshot to be taken
+    */
+   public static func makeScreenShot(name: String, testCase: XCTestCase?/*app: XCUIApplication, */) {
+      let screenshot = XCUIScreen.main.screenshot()
+      // let screenshot = app.windows.firstMatch.screenshot()
+      let attachment = XCTAttachment(screenshot: screenshot)
+      #if os(iOS)
+      attachment.name = "Screenshot-\(name)-\(UIDevice.current.name).png"
+      #else
+      attachment.name = "Screenshot-\(name)-macOS.png"
+      #endif
+      attachment.lifetime = .keepAlways
+      testCase?.add(attachment)
+   }
 }
 #endif
