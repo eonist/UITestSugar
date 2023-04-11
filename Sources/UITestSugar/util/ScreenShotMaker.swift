08e9d2070e21235b229843fc959c6af82ed4430e
eonist <30n1st@gmail.com>
2023-04-11 14:46:30 +0200

    Update ScreenShotMaker.swift

--- Sources/UITestSugar/util/ScreenShotMaker.swift
+++ Sources/UITestSugar/util/ScreenShotMaker.swift
@@ -5,13 +5,14 @@ import XCTest
 public final class ScreenShotMaker {
    /**
     * Screen-shot for iOS / macOS
+    * - Description: Take a screenshot of a given app and add it to the test attachements.
     * - Remark: You can find screenshots in Xcode -> Report navigator -> select your test
     * - Remark: Or go to: DerivedData -> ProjectName... -> Logs -> Test -> find .xcresult -> Show Package Contents -> Attachments
     * - Remark: Or search for the file `Screenshot` in deriveddata root folder
     * - Note: Ref: https://stackoverflow.com/a/56345842/5389500
-    * - Note: There is a xcode gallary feature to browse screenshots see: https://stackoverflow.com/a/74678917/5389500
+    * - Note: There is a xcode gallery feature to browse screenshots see: https://stackoverflow.com/a/74678917/5389500
     * - Note: https://www.appsdeveloperblog.com/xcuiscreenshot-creating-screenshots-in-ui-test/
-    * Take a screenshot of a given app and add it to the test attachements.
+    * - Fixme: ⚠️️ clean up comments
     * - Parameters:
     *   - testCase: Needed to add the attachment (is optional to avoid guard in caller etc)
     *   - app: The app to take a screenshot of.
@@ -21,12 +22,36 @@ public final class ScreenShotMaker {
     * ScreenShotMaker.makeScreenShot(testCase: self) // Put this line in your UITests where you want the screenshot to be taken
     */
    @discardableResult public static func makeScreenShot(name: String, testCase: XCTestCase?, app: XCUIApplication? = nil, useWin: Bool = false) -> XCUIScreenshot? {
+      if useWin {
+         guard let win: XCUIElement = app?.windows.firstMatch else { Swift.print("⚠️️ Err, ScreenShotMaker - No win"); return nil }
+         return screenShotWindow(name: name, testCase: testCase, window: win)
+      } else {
+         guard let app: XCUIApplication = app else { Swift.print("Err ⚠️️ ScreenShotMaker - No app"); return nil }
+         return makeScreenShot(name: name, testCase: testCase, app: app)
+      }
+   }
+   /**
+    * Screenshot app
+    */
+   public static func screenShotApp(name: String, testCase: XCTestCase?, app: XCUIApplication) -> XCUIScreenshot?  {
       guard let testCase = testCase else { Swift.print("⚠️️ Err, ScreenShotMaker.makeScreenShot() - testcase is nil"); return nil }
-      let screenshot: XCUIScreenshot = {
-         guard let app = app else { return XCUIScreen.main.screenshot() }
-         return useWin ? app.windows.firstMatch.screenshot() : app.screenshot()
-      }()
-      // let screenshot = app.windows.firstMatch.screenshot()
+      let screenshot = app.screenshot()
+      testCase.add(attachment(name: name, screenshot: screenshot)) // add screenshot to test directory
+      return screenshot
+   }
+   /**
+    * Screenshot window
+    */
+   @discardableResult static public func screenShotWindow(name: String, testCase: XCTestCase?, window: XCUIElement) -> XCUIScreenshot? {
+      guard let testCase = testCase else { Swift.print("⚠️️ Err, ScreenShotMaker.makeScreenShot() - testcase is nil"); return nil }
+      let screenshot = window.screenshot()
+      testCase.add(attachment(name: name, screenshot: screenshot)) // add screenshot to test directory
+      return screenshot
+   }
+   /**
+    * Attachment
+    */
+   fileprivate static func attachment(name: String, screenshot: XCUIScreenshot) -> XCTAttachment {
       let attachment = XCTAttachment(screenshot: screenshot)
       #if os(iOS)
       attachment.name = "Screenshot-\(name)-\(UIDevice.current.name).png"
@@ -34,8 +59,7 @@ public final class ScreenShotMaker {
       attachment.name = "Screenshot-\(name)-macOS.png"
       #endif
       attachment.lifetime = .keepAlways
-      testCase.add(attachment)
-      return screenshot
+      return attachment
    }
 }
 #endif
